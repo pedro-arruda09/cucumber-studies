481e8c8c665395185d1691772056bb0e54db03ce
pedro arruda <pedroarruda10005@gmail.com>
2023-01-07 15:00:00 -0300

    learning more about cucumber bdd and gherkin

---
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+When('I type a first name', () => {
+    cy.get('[name="first_name"]').type('Joe');
+});
+
+When('I type a last name', () => {
+    cy.get('[name="last_name"]').type('Blogs');
+});
+
+When('I enter an email address', () => {
+    cy.get('[name="email"]').type('joe_blogs123@mail.com');
+});
+
+When('I type a comment', () => {
+    cy.get('textarea[name="message"]').type('Hello world!');
+});
+
+When('I click on the submit button', () => {
+    cy.get('[type="submit"]').click();
+});
+
+Then('I should be presented with a succesful contact us submission message', () => {
+    cy.get('h1').should('have.text', 'Thank You for your Message!');
+});
+
+Then('I should be presented with a unsuccesful contact us submission message', () => {
+    cy.get('body').contains('Error: Invalid email address');
+});
+
+When('I type a specific first name {string}', firstName => {
+    cy.get('[name="first_name"]').type(firstName);
+});
+
+When('I type a specific last name {string}', lastName => {
+    cy.get('[name="last_name"]').type(lastName);
+});
+
+When('I type a specific email address {string}', email => {
+    cy.get('[name="email"]').type(email);
+});
+
+When('I type a specific word {string} and number {int} within the comment input field', (word, number) => {
+    cy.get('[name="message"]').type(word + number);
+});
+
+When('I type a first name {string} and a last name {string}', (firstName, lastName) => {
+    cy.get('[name="first_name"]').type(firstName);
+    cy.get('[name="last_name"]').type(lastName);
+});
+
+When('I type a {string} and a comment {string}', (email, comment) => {
+    cy.get('[name="email"]').type(email);
+    cy.get('[name="message"]').type(comment);
+});
+
+Then('I should be presented with header text {string}', message => {
+    cy.xpath('//h1 | //body').contains(message);
+});
